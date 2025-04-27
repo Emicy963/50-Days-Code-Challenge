b70cedc8e9c1d1f9ca2b5e4b25f74e7e97322801
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-04-27 22:54:19 +0100

    Solving the count word in string - 4th exercise

---
+++ src/ex4.py
@@ -0,0 +1,13 @@
+def cont_words(word: str)->dict:
+    words = dict
+    for i in word:
+        count_word = 0
+        if i in words.keys:
+            pass
+        words.keys = i
+        for j in word:
+            if i==j:
+                count_word+=1
+        words.values = count_word
+        count_word = 0
+    return words
