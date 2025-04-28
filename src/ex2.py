08f2562b9df81eae01886d442eaa7767e502983e
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-04-29 00:03:12 +0100

    Fixing sintax error in 2nd exercise

--- src/ex2.py
+++ src/ex2.py
@@ -1,4 +1,4 @@
 def string_invertor(word: str)->str:
-    pos = len(str)
-    string = [word[pos-i] for i in range(1, len(str)+1)]
+    pos = len(word)
+    string = [word[pos-i] for i in range(1, len(word)+1)]
     return "".join(string)
