b68f21baf7230e5ec802331d70666ffa428c888b
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-04-28 23:49:04 +0100

    Solving the bracket validator - 5th exercise

--- src/ex5.py
+++ src/ex5.py
@@ -1,10 +1,14 @@
 def bracket_valitor(word:str)->bool:
-    if '(' and ')' in word:
-        for i in range(len(word)//2):
-            if word[i] == ')':
+    if '(' in word and ')' in word:
+        bracket = []
+        for i in word:
+            if i=='(':
+                bracket.append(i)
+            if i==')' and len(bracket)==0:
                 return False
-            elif word[i] == '(':
-                pos = len(word)
-                for i, j in enumerate(word, start=1):
-                    if word[pos-i]==')'
+            elif i==')' and len(bracket)!=0:
+                bracket.pop()
+        return False if bracket else True
+    else:
+        return False
     
