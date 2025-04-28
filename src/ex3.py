06ed153fe12d6d4bcca890c7920a4b6f19c68c18
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-04-29 00:08:16 +0100

    Solving the palindroms exercise using two points - 3th exercise

--- src/ex3.py
+++ src/ex3.py
@@ -1,4 +1,16 @@
 from ex2 import string_invertor
 
 def palindromo(word:str)->bool:
-    return True if word==string_invertor(word) else False
+    return word==string_invertor(word)
+
+def palindroms_using_points(word:str)->bool:
+    left = 0
+    right = len(word)-1
+
+    while left<right:
+        if word[left]!=word[right]:
+            return False
+        left+=1
+        right-=1
+
+    return True
