1181690a5e865e492cb0833fe1cec05e6ddb94e2
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-03 20:43:19 -0400

    Update ex1.py
    is_integer is not string method. And "return" only once

--- src/ex1.py
+++ src/ex1.py
@@ -2,8 +2,10 @@ def cpf_verify(cpf: str)->bool:
     if len(cpf)!=11:
         return False
     else:
+        cont = 0
         for i in cpf:
-            if i.is_integer():
-                return True   
+            if not i.isdigit():
+                continue   
             else:
-                return False
+                cont += 1
+        return True if cont == 9 else False
