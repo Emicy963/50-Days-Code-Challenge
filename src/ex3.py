dcfd455f030c421d22a9a7be529a4bfc18894d0e
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-03 21:07:10 -0400

    Update ex3.py

--- src/ex3.py
+++ src/ex3.py
@@ -1,16 +1,15 @@
-from ex2 import string_invertor
+from ex2 import string_invertor, string_inverter
 
 def palindromo(word:str)->bool:
-    return word==string_invertor(word)
+    return word==word[::-1]
 
 def palindroms_using_points(word:str)->bool:
     left = 0
     right = len(word)-1
 
-    while left<right:
+    while right:
         if word[left]!=word[right]:
             return False
         left+=1
         right-=1
-
     return True
