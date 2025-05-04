c565a6cb218ba148e91546cc805d13c38fc72c51
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-03 21:13:56 -0400

    Update ex4.py

--- src/ex4.py
+++ src/ex4.py
@@ -1,13 +1,3 @@
 def cont_words(word: str)->dict:
-    words = dict()
-    for i in word:
-        count_word = 0
-        if i in words.keys:
-            pass
-        words.keys = i
-        for j in word:
-            if i==j:
-                count_word+=1
-        words.values = count_word
-        count_word = 0
-    return words
+    word = word.strip()
+    return len(word.split())
