390b304223effc02389745b33c8f15e737a4c0eb
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-09 21:48:00 +0100

    Solving the generator password - 16th exercise

---
+++ src/Validation and Algorithms/ex16.py
@@ -0,0 +1,23 @@
+def gerar_senhas(caracteres:dict, comprimento:int, criterios:list)->str:
+    import random
+    if not criterios:
+        return "Selecione ao menos um critério."
+
+    caracteres_possiveis = ""
+    for criterio in criterios:
+        caracteres_possiveis += caracteres[criterio]
+
+    senha = [random.choice(caracteres[criterio]) for criterio in criterios]
+
+    senha += [random.choice(caracteres_possiveis) for _ in range(comprimento - len(senha))]
+
+    random.shuffle(senha)
+
+    return "".join(senha)
+
+def salvar_senha(senhas:list, nome_do_arquivo="minhas_senhas.txt")->None:
+    with open(nome_do_arquivo, 'a') as arquivo:
+        for senha in senhas:
+            arquivo.write(senha + '\n')
+    print("Senha salva com sucesso!")
+    
