b04479144da090808eea9e281097382b900bf66b
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-18 23:37:40 +0100

    Solving some bugs in ex21.py and improving docstrings

--- src/3 - POO/ex21.py
+++ src/3 - POO/ex21.py
@@ -6,18 +6,43 @@ class ContaBancaria:
 
     # Método para consultar o saldo disponível
     def consultar_dinheiro(self) -> str:
+        """
+        Método para verificar o saldo disponível na conta
+
+        Returns:
+            String com o valor disponível na conta
+        """
         return f"Saldo disponível: {self.amount}"
 
     # Método para depositar dinheiro na conta
     def depositar_dinheiro(self, value: int) -> str:
+        """
+        Método que faz o deposito na conta
+
+        Args:
+            value: Valor a depositar
+
+        Returns:
+            String a dizer se o deposito foi feito com sucesso ou falhou
+        """
         try:
             self.amount += value  # Adiciona o valor ao saldo atual
-            return f"Deposito feito com sucesso!\n{self.consultar_dinheiro}"  # Retorna mensagem de sucesso
+            return f"Deposito feito com sucesso!\n{self.consultar_dinheiro()}"  # Retorna mensagem de sucesso
         except Exception as err:
             return f"Erro ao depositar valor.\nErro:{str(err)}"  # Retorna mensagem de erro caso ocorra uma exceção
 
     # Método para levantar dinheiro da conta
     def levantar_dinheiro(self, value: int) -> str:
+        """
+        Método que faz o levantamento de dinheiro na conta.
+        Este método verifica se o valor a levantar não é maior que o valor disponível na conta
+
+        Args:
+            value: Valor a levantar
+
+        Returns:
+            String a dizer se o levantamento foi feito com sucesso ou falhou
+        """
         try:
             if value <= self.amount:  # Verifica se há saldo suficiente
                 self.amount -= value  # Subtrai o valor do saldo atual
@@ -29,11 +54,21 @@ class ContaBancaria:
 
     # Método para transferir dinheiro para outra conta
     def transferir_dinheiro(self, value: int) -> str:
+        """
+        Método que faz o levantamento de dinheiro na conta.
+        Este método verifica se o valor a transferir não é maior que o valor disponível na conta
+
+        Args:
+            value: Valor a transferir
+
+        Returns:
+            String a dizer se o levantamento foi feito com sucesso ou falhou
+        """
         try:
             if value <= self.amount:  # Verifica se há saldo suficiente
                 self.amount -= value  # Subtrai o valor do saldo atual
                 self.account_to_send_amount += value  # Adiciona o valor ao saldo da conta de destino
-                return f"Dinheiro trasnferido com sucesso.\n{self.consultar_dinheiro}"  # Retorna mensagem de sucesso
+                return f"Dinheiro transferido com sucesso.\n{self.consultar_dinheiro}"  # Retorna mensagem de sucesso
             else:
                 return f"Saldo insuficiente.\n{self.consultar_dinheiro}"  # Retorna mensagem de saldo insuficiente
         except Exception as err:
