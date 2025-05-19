307f2cb9efab96fa78bdf7c22318f97ba76294dc
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-19 23:31:53 +0100

    Solving the first django challenge - 26th exercise

--- src/4-Django/clients/models.py
+++ src/4-Django/clients/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=100)
+    email = models.EmailField(verbose_name='Email')
+    age = models.PositiveIntegerField(verbose_name='Age')
+
+    def __str__(self):
+        return f'Client: {self.name}'
