dd9df4b546efd8de00d84274dc7acde77be7dccf
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-20 23:09:03 +0100

    Working in validation age in Client model - 27th exerice

--- src/4-Django/clients/models.py
+++ src/4-Django/clients/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Client(models.Model):
     name = models.CharField(verbose_name='Name', max_length=100)
@@ -7,3 +8,11 @@ class Client(models.Model):
 
     def __str__(self):
         return f'Client: {self.name}'
+
+    def verify_age(self):
+        """
+        Verify if client is at leat 18 years old.
+        Raises ValidationError if Client is underage.
+        """
+        if self.age<18:
+            raise ValidationError('We do not accept clients under 18 years old.')
