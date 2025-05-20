7e72da7ad910d9f8b965680bb4960ca5ceb34ddd
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-20 23:32:01 +0100

    Start test of Client model - Extra 27th exercise

--- src/4-Django/clients/tests.py
+++ src/4-Django/clients/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Client
+
+class ClientModelTest(TestCase):
+    def setUp(self):
+        """
+        Configurações iniciais para testar o Client
+        Criando alguns clientes para testar
+        """
+        # Cliente válido idade > 18
+        self.validation_client = Client.objects.create(
+            name='João Silva',
+            email='joao@exemple.com',
+            age=25
+        )
