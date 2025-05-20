d23e09b753c4000e3a1e56fa612ce5c51adea101
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-20 23:34:43 +0100

    Test the Create a Valid Client - Extra 27th exercise

--- src/4-Django/clients/tests.py
+++ src/4-Django/clients/tests.py
@@ -14,3 +14,19 @@ class ClientModelTest(TestCase):
             email='joao@exemple.com',
             age=25
         )
+    
+    def test_create_valid_client(self):
+        """
+        Testar a criação de um cliente válido.
+        """
+        client = Client.objects.create(
+            name='Maria Joana',
+            email='maria@example.com',
+            age=30
+        )
+        client.full_clean() # Não deve retornar nenhum erro
+        client.save()
+
+        # Verificar se foi salvo corretamente
+        self.assertEqual(Client.objects.count(), 2)
+        self.assertEqual(Client.objects.get(email='maria@example.com').name, 'Maria Joana')
