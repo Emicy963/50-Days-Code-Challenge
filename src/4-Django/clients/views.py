eb5acb77fd8fc48baaf55ec408a1ab91bbc2b720
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-21 15:06:43 +0100

    Create a POST method to C Create from CRUD - 28th exercise

--- src/4-Django/clients/views.py
+++ src/4-Django/clients/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Client
+from django.core.exceptions import ValidationError
+from django.contrib import messages
 
 def create_client(request):
     """
@@ -9,4 +11,21 @@ def create_client(request):
         # Se a requisição for do tipo GET, retornar o templates que mostra os clientes cadastrados
         clients = Client.objects.all()
         return render(request, 'clients.html', {'clients':clients})
+    else:
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        age=request.POST.get('age')
+        # Criar o cliente
+        client = Client.objects.create(
+            name=name,
+            email=email,
+            age=age
+        )
 
+        try:
+            client.save()
+            messages.success(request, 'Cliente criado com sucesso!')
+            return redirect('clients.html')
+        except ValidationError as err:
+            messages.error(request, f'Erro de validação: {str(err)}')
+            return redirect('create_client.html')
