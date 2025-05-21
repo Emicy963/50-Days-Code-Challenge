2d0d7c7503339a22c4d9ae89a00cdebcfc509d00
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-21 14:57:16 +0100

    Create a GET request R from CRUD - 28th exercise

--- src/4-Django/clients/views.py
+++ src/4-Django/clients/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Client
 
-# Create your views here.
+def create_client(request):
+    """
+    Método para criar usuário apartir do tipo da requisição
+    """
+    if request.method=='GET':
+        # Se a requisição for do tipo GET, retornar o templates que mostra os clientes cadastrados
+        clients = Client.objects.all()
+        return render(request, 'clients.html', {'clients':clients})
+
