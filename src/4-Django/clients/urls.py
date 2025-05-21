0f3f521d4f6df43f1ec26baf58e3e12f7720ec32
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-22 00:55:08 +0100

    Create a urls to views - 28th exercise

--- src/4-Django/clients/urls.py
+++ src/4-Django/clients/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.get_clients, name='clients'),
+    path('create_client/', views.create_client, name='create_client'),
+    path('update_client/<id>', views.update_client, name='update_client'),
+    path('delete_client/<id>', views.delete_client, name='delete_client'),
+    path('detail_client/<id>', views.detail_client, name='detail_client')
 ]
