2891051a01c6d18591ad90fea9c8a98a6fe464b9
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-23 00:14:57 +0100

    Add pagination in view that take all users - 29th exercise

--- src/4-Django/clients/views.py
+++ src/4-Django/clients/views.py
@@ -2,33 +2,44 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Client
 from django.core.exceptions import ValidationError
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 def get_clients(request):
     """
-    Método para fazer o pegar os clientes cadastrados
+    Método para pegar os clientes cadastrados com paginação
     """
-    if request.method=='GET':
-        clients = Client.objects.all() # Pegar todos os clientes cadastrados
-        return render(request, 'clients.html', {'clients': clients})
-    
+    if request.method == 'GET':
+        clients_list = Client.objects.all().order_by('name')  # Ordenar por nome
+        
+        # Configurar paginação - 10 clientes por página
+        paginator = Paginator(clients_list, 10)
+        page_number = request.GET.get('page')
+        clients = paginator.get_page(page_number)
+        
+        return render(request, 'clients.html', {
+            'clients': clients,
+            'total_clients': clients_list.count()
+        })
+
 def create_client(request):
     """
     Método para criar usuário apartir do tipo da requisição
     """
-    if request.method=='GET':
+    if request.method == 'GET':
         return render(request, 'create_client.html')
     else:
-        name=request.POST.get('name')
-        email=request.POST.get('email')
-        age=request.POST.get('age')
-        # Salvar o cliente e levantar erros
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        
         try:
             # Criar o cliente
             client = Client.objects.create(
                 name=name,
                 email=email,
-                age=int(age)
+                age=int(age)  # Converter para inteiro
             )
+            client.save()
             messages.success(request, 'Cliente criado com sucesso!')
             return redirect('clients')
         except ValidationError as err:
@@ -42,21 +53,20 @@ def update_client(request, id):
     """
     Método para atualizar um usuário
     """
-    client = get_object_or_404(Client, id=id) # Pegar o cliente que se pretende atualizar via ID
-
-    if request.method=='GET':
+    client = get_object_or_404(Client, id=id)  # Pegar o cliente que se pretende atualizar via ID
+    
+    if request.method == 'GET':
         return render(request, 'update_client.html', {'client': client})
     
-    if request.method=='POST':
-        # Pegar a informações do template
+    elif request.method == 'POST':
+        # Pegar as informações do template
         client.name = request.POST.get('name')
         client.email = request.POST.get('email')
         age = request.POST.get('age')
-
+        
         try:
-            # Salvar as atualizações do cliente
-            client.age = int(age)
-            client.save()
+            client.age = int(age)  # Converter para inteiro
+            client.save()  # Salvar as atualizações do cliente
             messages.success(request, 'Cliente atualizado com sucesso!')
             return redirect('detail_client', id=client.id)
         except ValidationError as err:
@@ -71,7 +81,7 @@ def delete_client(request, id):
     Método para apagar um cliente
     """
     client = get_object_or_404(Client, id=id)
-    client_name = client.name
+    client_name = client.name  # Guardar o nome para a mensagem
     client.delete()
     messages.success(request, f'Cliente {client_name} foi excluído com sucesso!')
     return redirect('clients')
