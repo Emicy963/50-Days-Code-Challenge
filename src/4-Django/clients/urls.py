7a380bf462ab4b052726b1d5cf01194d61356129
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-25 01:17:29 +0100

    Update the urls.py - 31th exercise

--- src/4-Django/clients/urls.py
+++ src/4-Django/clients/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('create_client/', views.create_client, name='create_client'),
     path('update_client/<int:id>/', views.update_client, name='update_client'),
     path('delete_client/<int:id>/', views.delete_client, name='delete_client'),
-    path('detail_client/<int:id>/', views.detail_client, name='detail_client')
+    path('detail_client/<int:id>/', views.detail_client, name='detail_client'),
+    path('bulk-delete/', views.bulk_delete_clients, name='bulk_delete_clients'),
 ]
