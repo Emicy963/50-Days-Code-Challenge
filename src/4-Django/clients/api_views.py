a1be8eec01c47cc8bcd6070ae13b09fd4062964f
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-06-05 23:33:50 +0100

    Start working in API Views - 41th exercise

---
+++ src/4-Django/clients/api_views.py
@@ -0,0 +1,7 @@
+from rest_framework.pagination import PageNumberPagination
+
+class StandardResultsSetPagination(PageNumberPagination):
+    """Paginação padrão para a API"""
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
