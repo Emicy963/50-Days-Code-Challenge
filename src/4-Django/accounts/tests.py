19e278b0eace9e0a4929aa4a06461882ceda911a
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-06-08 22:30:02 +0100

    Test if user be created - 43th exercise

--- src/4-Django/accounts/tests.py
+++ src/4-Django/accounts/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.contrib.messages import get_messages
 
 # TESTES PARA VIEWS TRADICIONAIS (TEMPLATES)
 
@@ -23,3 +24,25 @@ class AuthViewsTestCase(TestCase):
         
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'register.html')
+
+    def test_register_view_post_success(self):
+        """Testa registro bem-sucedido de usuário"""
+        data = {
+            'name': 'newuser',
+            'email': 'newuser@example.com',
+            'password': 'newpassword123',
+            'confirm_password': 'newpassword123'
+        }
+        
+        response = self.client.post(reverse('register'), data)
+        
+        # Verifica redirecionamento
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('clients'))
+        
+        # Verifica se usuário foi criado
+        self.assertTrue(User.objects.filter(username='newuser').exists())
+        
+        # Verifica mensagem de sucesso
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]), "Conta criada com sucesso!")
