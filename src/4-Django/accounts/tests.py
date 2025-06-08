25c2a6f4c7721e2cac1dfb97400efaa9596d9680
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-06-08 22:26:50 +0100

    Setup the test for normal views - 43th exercise

--- src/4-Django/accounts/tests.py
+++ src/4-Django/accounts/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
+# TESTES PARA VIEWS TRADICIONAIS (TEMPLATES)
+
+class AuthViewsTestCase(TestCase):
+    """Testes para views de autenticação tradicionais"""
+    
+    def setUp(self):
+        """Configuração inicial para cada teste"""
+        self.client = Client()
+        self.user_data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password': 'testpassword123'
+        }
+        self.user = User.objects.create_user(**self.user_data)
