c5251c5a388bed3f26588bdc9f54b9ae168b82ce
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-06-09 23:07:03 +0100

    Create the Meta class and str method - 44th exercise

--- src/4-Django/accounts/models.py
+++ src/4-Django/accounts/models.py
@@ -31,3 +31,10 @@ class UserProfile(models.Model):
     phone = models.CharField(max_length=20, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Perfil do Usuário"
+        verbose_name_plural = "Perfis dos Usuários"
+
+    def __str__(self):
+        return f"Perfil de {self.user.username}"
