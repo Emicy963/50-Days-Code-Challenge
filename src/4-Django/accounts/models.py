f64b39d79179219a60916b06d18a830cf37b4a09
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-06-09 23:04:08 +0100

    Create a fuction for define profile photo - 44th exercise

--- src/4-Django/accounts/models.py
+++ src/4-Django/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import os
 
-# Create your models here.
+def user_profile_photo_path(instance, filename):
+    """
+    Funcão para definir o caminho do arquivo de foto de perfil do usuário.
+    """
+    # Obter extensão do arquivo
+    ext = filename.split('.')[-1]
+    # Criar nome único baseado no ID do usuário
+    filename = f"user_{instance.user.id}_profile.{ext}"
+    return os.path.join('profile_photos', filename)
